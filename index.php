7884fbb93dafea90d062c629f0d8a732c7f6f146
Alberto Bernal <albera10@hotmail.com>
2023-09-07 00:06:43 -0600

    Se prepararon los directorios

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+	<title></title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="apple-mobbile-web-app-capable" content="yes">
+	<meta name="apple-mobbile-web-app-title" content="">
+	<link rel="icon" href="images/logo.png">
+	
+		<!-- Marcado HTML5 -->
+	<meta name="title" content="USERS">
+	<meta name="description" content="Administración de usuarios">
+	<meta name="keyword" content="suers, perfil, web"> 
+	
+	   <!-- CSS STYLES -->	   
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="normalize/normalize.css">
+    <link rel="stylesheet" href="materialize/css/materialize.min.css">	
+	<link rel="stylesheet" href="css/main.css">
+
+	<script src="materialize/js/materialize.min.js"></script>
+</head>
+<body>
+
+	<!-- Cabeza -->
+    <header>
+		<nav class="pink lighten-2">
+			<div class="container">
+				<div class="nav-wrapper">
+					<a href="#!" class="brand-logo">INE</a>
+					<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+					<ul class="right hide-on-med-and-down"> <!-- right hide-on-med-and-down -->
+						<li><a href="sass.html">Artículos</a></li>
+						<li><a href="badges.html">Registro</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		
+		<!-- Menu para mobiles -->
+		<ul class="sidenav" id="mobile-demo">
+			<li><a href="sass.html">Artículos</a></li>
+			<li><a href="badges.html">Registro</a></li>		
+		</ul>
+	</header>
+
+	<!-- Cuerpo -->
+	<div class="container">
+
+	</div>
+	
+
+
+
+
+       <!-- JAVASCRIPT SCRIPTS -->    
+    <script src="js/app.js"></script>
+</body>
+</html>
